36feaaa3548dc4bd1f7fe45fc161d1d2f86b4840
Илья Юрьевич Алексеенков <74237440+Alexeenkov@users.noreply.github.com>
2020-12-27 16:10:43 +0300

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,6 @@
 let header__burger = document.querySelector('.header-mobile__burger');
 let header_menu = document.querySelector('.header-mobile__menu');
-let header__link = document.querySelector('.header-mobile__link');
+let header__link = document.querySelectorAll('.header-mobile__link');
 
 header__burger.onclick = function() {
     header__burger.classList.toggle('active');
