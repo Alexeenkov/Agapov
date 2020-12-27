4f215a25dfa7d22ae3ecea65fedbb936bb520913
Илья Юрьевич Алексеенков <74237440+Alexeenkov@users.noreply.github.com>
2020-12-27 16:43:18 +0300

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
-    $('.header-mobile__burger').click(function(event) {
-        $('.header-mobile__burger,.header-mobile__menu').toggleClass('active');
-        $('body').toggleClass('burger-lock');
-    });
-});
+let header__burger = document.querySelector('.header-mobile__burger');
+let header_menu = document.querySelector('.header-mobile__menu');
+let header__link = document.querySelectorAll('.header-mobile__link');
+
+header__link.onclick = function() {
+    header__burger.classList.remove('active');
+    header_menu.classList.remove('active');
+    body.classList.remove('burger-lock');
+};
+
+header__burger.onclick = function() {
+    header__burger.classList.toggle('active');
+    header_menu.classList.toggle('active');
+    body.classList.toggle('burger-lock');
+};
